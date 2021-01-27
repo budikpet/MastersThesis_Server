142f927850832141c31ba5c67bc430ff5ee005df
budikpet <sgt.petrov@gmail.com>
2021-01-27 22:27:56 +0100

    Update clock.py
    Job is now run every 45 min to test sleeping.

--- clock.py
+++ clock.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=45)
 def timed_job():
-    print(f'This job is run every 1 minute. Current time is: {datetime.now()}')
+    print(f'This job is run every 45 minute. Current time is: {datetime.now()}')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
