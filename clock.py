fc82b14d8f6c567ccd00507930fa0f75c3052c8a
budikpet <sgt.petrov@gmail.com>
2021-01-28 12:04:55 +0100

    Update clock.py
    Added other schedulers and debugging.

--- clock.py
+++ clock.py
@@ -1,5 +1,9 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+import logging
+
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 sched = BlockingScheduler()
 
@@ -7,8 +11,16 @@ sched = BlockingScheduler()
 def timed_job():
     print(f'This job is run every 45 minute. Current time is: {datetime.now()}')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+# def scheduled_job():
+#     print('This job is run every weekday at 5pm.')
+
+@sched.scheduled_job('cron', minute=0, hour=22)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print('This job is run every day at 22:00.')
+
+@sched.scheduled_job('cron', day = 'last', minute=0, hour=22)
+def scheduled_job():
+    print('This job is run on a last day of the month at 21:00.')
 
 sched.start()
