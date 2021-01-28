9b8d3fb66da8fc0156ba0419084e889b09bc18e6
budikpet <sgt.petrov@gmail.com>
2021-01-28 14:34:01 +0100

    Update clock.py
    Bugfix

--- clock.py
+++ clock.py
@@ -13,23 +13,23 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print(f'JOB EXECUTED: This job is run every minute. Current time is: {datetime.now()}')
+	print(f'JOB EXECUTED: This job is run every minute. Current time is: {datetime.now()}')
 	result = q.enqueue(zoo_scrapper.run_test_job)
 	print(f'POST JOB: {result}')
 
 @sched.scheduled_job('interval', minutes=45)
 def timed_job():
-    print(f'JOB EXECUTED: This job is run every 45 minute. Current time is: {datetime.now()}')
+	print(f'JOB EXECUTED: This job is run every 45 minute. Current time is: {datetime.now()}')
 
 @sched.scheduled_job('cron', minute=0, hour=2)
 def scheduled_job():
-    print('JOB EXECUTED: This job is run every day at 22:00.')
+	print('JOB EXECUTED: This job is run every day at 22:00.')
 
 @sched.scheduled_job('cron', day = 'last', minute=0, hour=22, misfire_grace_time=None)
 def scheduled_job():
 	"""
 	misfire_grace_time=None should make it certain that the job isn't discarted if scheduled execution is missed
 	"""
-    print('JOB EXECUTED: This job is run on a last day of the month at 21:00.')
+	print('JOB EXECUTED: This job is run on a last day of the month at 21:00.')
 
 sched.start()
