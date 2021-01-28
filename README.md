98f7940270b155462ad4056d85f3ddf886e718ff
Petr B <Sgt.Petrov@gmail.com>
2021-01-28 18:06:15 +0100

    Clock test (#1)
    Tested Heroku scheduling using APScheduler.
    Tested basic background worker using requests, beautifulsoup, redis and rq.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Masters thesis server
+
+  
+## Create the server in Heroku
+The server uses a [custom clock process](https://devcenter.heroku.com/articles/scheduled-jobs-custom-clock-processes) and a [background worker](https://devcenter.heroku.com/articles/python-rq) which enable scheduling of long running tasks. It's used here primarily for automatic web scrapping of Zoo Prague lexicon and downloading updated OSM map tiles. 
---
+++ clock.py
@@ -0,0 +1,31 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+from rq import Queue
+from worker import conn
+import zoo_scrapper
+
+import logging
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
+q = Queue(connection=conn)
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=45)
+def timed_job():
+	print(f'JOB EXECUTED: This job is run every 45 minute. Current time is: {datetime.now()}')
+	result = q.enqueue(zoo_scrapper.run_test_job)
+
+@sched.scheduled_job('cron', minute=0, hour=2)
+def scheduled_job():
+	print('JOB EXECUTED: This job is run every day at 22:00.')
+	result = q.enqueue(zoo_scrapper.run_test_job)
+
+@sched.scheduled_job('cron', day = 'last', minute=0, hour=22, misfire_grace_time=None)
+def scheduled_job():
+	"""
+	misfire_grace_time=None should make it certain that the job isn't discarted if scheduled execution is missed
+	"""
+	print('JOB EXECUTED: This job is run on a last day of the month at 21:00.')
+
+sched.start()
