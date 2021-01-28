c55e63b22638d7c45436096ccee064ded8495e2b
budikpet <sgt.petrov@gmail.com>
2021-01-28 14:45:14 +0100

    Update clock.py
    Clock process 1/min with background work tested.
    Moving to long running test of 1/45min.

--- clock.py
+++ clock.py
@@ -11,19 +11,15 @@ logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 q = Queue(connection=conn)
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-	print(f'JOB EXECUTED: This job is run every minute. Current time is: {datetime.now()}')
-	result = q.enqueue(zoo_scrapper.run_test_job)
-	print(f'POST JOB: {result}')
-
 @sched.scheduled_job('interval', minutes=45)
 def timed_job():
 	print(f'JOB EXECUTED: This job is run every 45 minute. Current time is: {datetime.now()}')
+	result = q.enqueue(zoo_scrapper.run_test_job)
 
 @sched.scheduled_job('cron', minute=0, hour=2)
 def scheduled_job():
 	print('JOB EXECUTED: This job is run every day at 22:00.')
+	result = q.enqueue(zoo_scrapper.run_test_job)
 
 @sched.scheduled_job('cron', day = 'last', minute=0, hour=22, misfire_grace_time=None)
 def scheduled_job():
