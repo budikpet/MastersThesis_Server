771125bd0d74ebd48d947f346dcf2542e35526ff
budikpet <sgt.petrov@gmail.com>
2021-01-30 20:42:22 +0100

    Interfaces and subclasses are now connected.
    Subclasses of all interfaces are now provided using entry_points and loaded when interfaces are imported. That means interfaces always know their subclasses.
    
    Tests, mongodb_handler and official_data_collector ready for implementation.

--- src/scrapers/__main__.py
+++ src/scrapers/__main__.py
@@ -1,2 +1,5 @@
+from .zoo_scraper import main
+
 # This code is run when "python -m scrapers" is invoked. Not run when "scrapers" entry_point is used
 print("Entry point for the web scraper script")
+main()
