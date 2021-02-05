a9f94f14b14d560f29fe1c399c72b316851e378e
budikpet <sgt.petrov@gmail.com>
2021-02-05 16:07:46 +0100

    Base test done.
    Removed unused JSON files.

--- src/server_dataclasses/models.py
+++ src/server_dataclasses/models.py
@@ -5,47 +5,47 @@ from dataclasses import dataclass, field
 class AnimalData():
     """Holds data about animal parsed from Zoo Prague lexicon."""
 
-    # Parsed data
+    ### Parsed data
     id: int = -1
-    name: str = ''
-    latin_name: str = ''
+    name: str = None
+    latin_name: str = None
 
     # Text above the image
-    base_summary: str = ''
-    image: str = ''
+    base_summary: str = None
+    image: str = None
 
     # "Třída"
-    class_: str = ''
-    class_latin: str = ''
+    class_: str = None
+    class_latin: str = None
 
     # "Řád"
-    order: str = ''
-    order_latin: str = ''
+    order: str = None
+    order_latin: str = None
 
     # "Rozšíření"
-    continent: str = ''
-    continent_detail: str = ''
+    continent: str = None
+    continent_detail: str = None
 
-    biotop: str = ''
-    biotop_detail: str = ''
+    biotop: str = None
+    biotop_detail: str = None
 
     # "Potrava"
-    food: str = ''
-    food_detail: str = ''
+    food: str = None
+    food_detail: str = None
 
-    sizes: str = ''
-    reproduction: str = ''
+    sizes: str = None
+    reproduction: str = None
 
     # "Zajímavosti"
-    interesting_data: str = ''
+    interesting_data: str = None
 
     # "Chov v Zoo Praha"
-    about_placement_in_zoo_prague: str = ''
+    about_placement_in_zoo_prague: str = None
 
     # "Umístění v Zoo Praha"
-    location_in_zoo: str = ''
+    location_in_zoo: str = None
 
-    # Other data
+    ### Other data
 
     # False for animals that have an indication in about_placement_in_zoo_prague variable
     is_currently_available: bool = True
