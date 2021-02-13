56cc72ec05c414f4a0905373a0317e71564a9798
Petr B <Sgt.Petrov@gmail.com>
2021-02-13 12:46:26 +0100

    Testing worker burst mode.

--- src/scheduler/worker.py
+++ src/scheduler/worker.py
@@ -3,11 +3,13 @@ import redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:5c71930714e02c197e8d320eaccf66fa@soapfish.redistogo.com:10606/')
 conn = redis.from_url(redis_url)
 
 
 def main():
     with Connection(conn):
         worker = Worker(map(Queue, listen))
-        worker.work()
+        print("Running worker")
+        worker.work(burst=True)
+        print("Worker done")
