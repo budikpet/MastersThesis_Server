8f4b272c4660d8af6161bc32aab2dce01dd22193
Petr B <Sgt.Petrov@gmail.com>
2021-02-15 16:17:56 +0100

    Enhanced MongoDBHandler and its interface.
    Added 2 new methods.
    Started using these methods.

--- src/scrapers/mongodb_handler.py
+++ src/scrapers/mongodb_handler.py
@@ -1,5 +1,8 @@
 from server_dataclasses.interfaces import DBHandlerInterface
-import pymongo
+from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection
+import os
 
 
 class MongoDBHandler(DBHandlerInterface):
@@ -12,9 +15,10 @@ class MongoDBHandler(DBHandlerInterface):
     name: str = 'mongodb'
 
     def __init__(self, host: str, db_name: str, collection_name: str, **kwargs):
-        self.client = pymongo.MongoClient(host)
-        self.db = self.client[db_name]
-        self.coll = self.db[collection_name]
+        url = os.getenv('MONGODB_URI', host)
+        self.client: MongoClient = MongoClient(url)
+        self.db: Database = self.client[db_name]
+        self.coll: Collection = self.db[collection_name]
 
     def __enter__(self):
         return self
@@ -26,9 +30,52 @@ class MongoDBHandler(DBHandlerInterface):
         # suppress errors
         return True
 
-    def insert_many(self, data: list, **kwargs) -> bool:
+    def insert_many(self, data: list, db_name: str = None, collection_name: str = None, **kwargs) -> bool:
         """Collects Zoo Prague lexicon data and stores it in a DB."""
+        db: Database = self.db if db_name is None else self.client[db_name]
+        coll: Collection = self.coll if collection_name is None else db[collection_name]
+        
         dicts = [animal.__dict__ for animal in data]
-        self.coll.insert_many(dicts)
+        coll.insert_many(dicts)
+
+        return True
+
+    def insert_one(self, data: dict, db_name: str = None, collection_name: str = None, **kwargs) -> bool:
+        """
+        Collects one thing and stores it in a DB.
+
+        Args:
+            data (dict): Data to store
+            db_name (str, optional): Name of the database where the collection is. Defaults to None.
+            collection_name (str, optional): Name of the collection where to put data to. Defaults to None.
+
+        Returns:
+            bool: [description]
+        """
+        db: Database = self.db if db_name is None else self.client[db_name]
+        coll: Collection = self.coll if collection_name is None else db[collection_name]
+
+        coll.insert_one(data)
+
+        return True
+
+    def update_one(self, filter_: dict, data: dict, upsert: bool = False, db_name: str = None, collection_name: str = None, **kwargs) -> bool:
+        """
+        Updates one document. If more then one document is found then only the first is updated.
+
+        Args:
+            filter_ (dict): Determines how to find the document to update.
+            data (dict): Determines how the document is updated.
+            upsert (bool, optional): If set to True and no document is found then a new document is created. Defaults to False.
+            db_name (str, optional): Name of the database where the collection is. Defaults to None.
+            collection_name (str, optional): Name of the collection where to put data to. Defaults to None.
+
+        Returns:
+            bool: [description]
+        """
+        db: Database = self.db if db_name is None else self.client[db_name]
+        coll: Collection = self.coll if collection_name is None else db[collection_name]
+
+        coll.update_one(filter_, data, upsert=upsert)
 
         return True
--- src/server_dataclasses/interfaces.py
+++ src/server_dataclasses/interfaces.py
@@ -22,6 +22,10 @@ class DBHandlerInterface(metaclass=abc.ABCMeta):
             callable(subclass.__enter__),
             hasattr(subclass, '__exit__'),
             callable(subclass.__exit__),
+            hasattr(subclass, 'update_one'),
+            callable(subclass.update_one),
+            hasattr(subclass, 'insert_one'),
+            callable(subclass.insert_one),
             hasattr(subclass, 'insert_many'),
             callable(subclass.insert_many)
         ]
@@ -36,8 +40,40 @@ class DBHandlerInterface(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def insert_many(self, data: list, **kwargs) -> bool:
-        """Collects Zoo Prague lexicon data and stores it in a DB."""
+    def insert_many(self, data: list, db_name: str = None, collection_name: str = None, **kwargs) -> bool:
+        """Collects a list and stores it in a DB"""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def insert_one(self, data: dict, db_name: str = None, collection_name: str = None, **kwargs) -> bool:
+        """
+        Collects one thing and stores it in a DB.
+
+        Args:
+            data (dict): Data to store
+            db_name (str, optional): Name of the database where the collection is. Defaults to None.
+            collection_name (str, optional): Name of the collection where to put data to. Defaults to None.
+
+        Returns:
+            bool: [description]
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def update_one(self, filter: dict, data: dict, upsert: bool = False, db_name: str = None, collection_name: str = None, **kwargs) -> bool:
+        """
+        Updates one document. If more then one document is found then only the first is updated.
+
+        Args:
+            filter_ (dict): Determines how to find the document to update.
+            data (dict): Determines how the document is updated.
+            upsert (bool, optional): If set to True and no document is found then a new document is created. Defaults to False.
+            db_name (str, optional): Name of the database where the collection is. Defaults to None.
+            collection_name (str, optional): Name of the collection where to put data to. Defaults to None.
+
+        Returns:
+            bool: [description]
+        """
         raise NotImplementedError
 
 
