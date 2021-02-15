795a5ce1e9b756f0b2f718a33a3b95b31dbc68dd
Petr B <Sgt.Petrov@gmail.com>
2021-02-15 11:16:13 +0100

    Added better logging.

--- src/scheduler/worker.py
+++ src/scheduler/worker.py
@@ -6,10 +6,26 @@ listen = ['high', 'default', 'low']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:5c71930714e02c197e8d320eaccf66fa@soapfish.redistogo.com:10606/')
 conn = redis.from_url(redis_url)
 
+# Define logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+
+file_handler = logging.FileHandler('log/errors.log')
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 def main():
     with Connection(conn):
         worker = Worker(map(Queue, listen))
-        print("Running worker")
+        logger.info("Running worker")
         worker.work(burst=True)
-        print("Worker done")
+        logger.info("Worker done")
